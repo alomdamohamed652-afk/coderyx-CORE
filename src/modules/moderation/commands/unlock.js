cbcb180e8a99fd222086b76f5dbfda4e0e313b69
alomdamohamed652-afk <alomdamohamed652@gmail.com>
2026-09-02 16:51:02 +0300

    Record unlock moderation case

--- src/modules/moderation/commands/unlock.js
+++ src/modules/moderation/commands/unlock.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder, PermissionFlagsBits, ChannelType } = require("discord.js");
 const { hasPermission } = require("../../../utils/permissions");
 const pendingActions = require("../../../utils/pendingActions");
+const caseManager = require("../../../utils/caseManager");
 
 const data = new SlashCommandBuilder()
   .setName("unlock")
@@ -24,6 +25,7 @@ async function execute(interaction) {
   try {
     pendingActions.record(`channelUpdate:${channel.id}`, { executor: interaction.user, reason });
     await channel.permissionOverwrites.edit(interaction.guild.roles.everyone, { SendMessages: true }, { reason });
+    caseManager.create({ guildId: interaction.guildId, action: "unlock", target: { id: channel.id, tag: channel.name }, moderator: interaction.user, reason, source: "command" });
     await interaction.editReply({ content: `🔓 تم فتح ${channel}.` });
   } catch (err) {
     await interaction.editReply({ content: `❌ ماقدرتش أفتح الروم: ${err.message}` });
